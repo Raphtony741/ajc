5af4ede6e1971833403736476e9d56761ba58862
Raphtony741 <114031436+Raphtony741@users.noreply.github.com>
2025-01-21 21:40:02 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,8 @@ export default function Home() {
     <>
       <Navbar />
       <main style={{ textAlign: 'center', padding: '2rem', backgroundColor: '#111', color: '#fff' }}>
-        <h1>Welcome to the Anti Jamf Club/Clan</h1>
-        <p>Join the revolution. Fear is power.</p>
+        <h1>Bienvenue sur Anti Jamf Club</h1>
+        <p>rejoin la révolution.</p>
       </main>
       <Footer />
     </>
