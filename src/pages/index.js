53b7290f75290a3b7abc00600afc0d79ec5aa13f
Raphtony741 <114031436+Raphtony741@users.noreply.github.com>
2025-01-21 22:20:31 -0500

    Update index.js
    En théorie, full screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,17 @@ export default function Home() {
         <title>Agissez - Anti Jamf Club</title>
       </Head>
       <Navbar />
-		<main style={{ textAlign: 'center', padding: '2rem', backgroundColor: '#111', color: '#fff' }}>
+		<main style={{ 
+  textAlign: 'center', 
+  padding: '2rem', 
+  backgroundColor: '#111', 
+  color: '#fff', 
+  height: '100vh', 
+  display: 'flex', 
+  flexDirection: 'column', 
+  justifyContent: 'center', 
+  alignItems: 'center' 
+	}}>
 			<h1 style={{ color: 'red' }}>Bienvenue sur Anti Jamf Club</h1>
 			<p style={{ color: 'red' }}>Rejoignez la révolution.</p>
 		</main>
