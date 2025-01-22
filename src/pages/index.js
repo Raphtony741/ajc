c64628adfef6183fbc64e8a59ddd66d6af5d5f5b
Raphtony741 <114031436+Raphtony741@users.noreply.github.com>
2025-01-21 22:03:59 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,17 @@
+import Head from 'next/head';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Agissez - Anti Jamf Club</title>
+      </Head>
       <Navbar />
       <main style={{ textAlign: 'center', padding: '2rem', backgroundColor: '#111', color: '#fff' }}>
         <h1>Bienvenue sur Anti Jamf Club</h1>
-        <p>rejoin la révolution.</p>
+        <p>Rejoignez la révolution.</p>
       </main>
       <Footer />
     </>
