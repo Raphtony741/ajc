0de3f20fdb845cf74b43e6330a87caa84cd55b25
Raphtony741 <114031436+Raphtony741@users.noreply.github.com>
2025-02-03 10:27:10 -0500

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,32 @@
-import '../../styles/globals.css';
-import '../i18n/i18n'; // Import your i18n configuration
+// src/pages/_app.js
+
+import { useState } from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import Popup from '../components/Popup'; // Importer le composant Popup
+import { useTranslation } from 'react-i18next';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const { t } = useTranslation();
+  const [showPopup, setShowPopup] = useState(true); // État pour gérer l'affichage du pop-up
+
+  const handleClosePopup = () => {
+    setShowPopup(false); // Fermer le pop-up
+  };
+
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+      {showPopup && (
+        <Popup 
+          message="Nouvelle méthode trouvée, date de sortie dans 5 jours." 
+          onClose={handleClosePopup} 
+        />
+      )}
+    </>
+  );
 }
 
 export default MyApp;
