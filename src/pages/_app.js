f6d04abdf6c01609f47f03e311e99f60c22898dc
Raphtony741 <114031436+Raphtony741@users.noreply.github.com>
2025-02-04 13:13:20 -0500

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -37,9 +37,6 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-    <head>
-    <meta property="og:image" content="https://ajcqc.info/favicon.ico" />
-    </head>
       <Component {...pageProps} />
       {showPopup && timeLeft && ( // Vérification de timeLeft
         <Popup 
