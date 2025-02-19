2767b93c837e64de8528079a1cea46ac9258681c
Raphtony741 <114031436+Raphtony741@users.noreply.github.com>
2025-02-19 09:04:11 -0500

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,12 +2,15 @@
 
 import '../../styles/globals.css';
 import '../i18n/i18n';
-import Popup from '../components/Popup'; 
 import { useTranslation } from 'react-i18next';
 
 function MyApp({ Component, pageProps }) {
   const { t } = useTranslation();
 
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
   );
 }
 
