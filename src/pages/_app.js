eab8156d1326bc06cfaff93ffb0a6562fc4a705f
Raphtony741 <114031436+Raphtony741@users.noreply.github.com>
2025-02-19 08:56:52 -0500

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,42 +10,6 @@ function MyApp({ Component, pageProps }) {
   const [showPopup, setShowPopup] = useState(true); 
   const [timeLeft, setTimeLeft] = useState(''); // Valeur par défaut
 
-  useEffect(() => {
-    const targetDate = new Date(Date.now() + 5 * 24 * 60 * 60 * 1000); // 5 jours à partir de maintenant
-
-    const interval = setInterval(() => {
-      const now = new Date();
-      const difference = targetDate - now;
-
-      if (difference <= 0) {
-        clearInterval(interval);
-        setTimeLeft('Le temps est écoulé !');
-      } else {
-        const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-        setTimeLeft(`${days} jours, ${hours} heures, ${minutes} minutes`);
-      }
-    }, 1000); // Met à jour chaque seconde
-
-    return () => clearInterval(interval);
-  }, []);
-
-  const handleClosePopup = () => {
-    setShowPopup(false); 
-  };
-
-  return (
-    <>
-      <Component {...pageProps} />
-      {showPopup && timeLeft && ( // Vérification de timeLeft
-        <Popup 
-          message={t('popup_message')} 
-          timeLeft={timeLeft} 
-          onClose={handleClosePopup} 
-        />
-      )}
-    </>
   );
 }
 
