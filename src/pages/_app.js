fe3dec572bd394bfe0e2c22542db39d7ac734be1
Raphtony741 <114031436+Raphtony741@users.noreply.github.com>
2025-02-19 09:02:27 -0500

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 // src/pages/_app.js
-import { useEffect, useState } from 'react';
+
 import '../../styles/globals.css';
 import '../i18n/i18n';
 import Popup from '../components/Popup'; 
@@ -7,8 +7,6 @@ import { useTranslation } from 'react-i18next';
 
 function MyApp({ Component, pageProps }) {
   const { t } = useTranslation();
-  const [showPopup, setShowPopup] = useState(true); 
-  const [timeLeft, setTimeLeft] = useState(''); // Valeur par défaut
 
   );
 }
